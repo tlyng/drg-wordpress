55e64d995bdb9ced9e5f0acea2d9070e5f4df485
Torkel Lyng <torkel.lyng@gmail.com>
2014-03-19 22:32:54 +0100

    Initial implementation of Logger

--- src/drg/wordpress/Logger.php
+++ src/drg/wordpress/Logger.php
@@ -1,5 +1,20 @@
 <?php
 namespace drg\wordpress;
 
-use Monolog\Logger as MonoLogger;
-use Monolog
+use \Monolog\Logger as MonoLogger;
+use \Monolog\Handler\StreamHandler;
+
+class Logger {
+    private function __construct() {
+    }
+
+    public static function getLogger($name) {
+        $logger = new MonoLogger($name);
+        $logger->pushHandler(new StreamHandler(WP_PLUGIN_DIR . "/logs/{$name}-info.log", MonoLogger::INFO));
+        $logger->pushHandler(new StreamHandler(WP_PLUGIN_DIR . "/logs/{$name}-warn.log", MonoLogger::WARNING));
+        $logger->pushHandler(new StreamHandler(WP_PLUGIN_DIR . "/logs/{$name}-error.log", MonoLogger::ERROR));
+        $logger->pushHandler(new StreamHandler(WP_PLUGIN_DIR . "/logs/{$name}-debug.log", MonoLogger::DEBUG));
+
+        return $logger;
+    }
+}
